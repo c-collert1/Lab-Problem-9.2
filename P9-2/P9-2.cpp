b8a816452d6bfef01d354c28a154d5818ae3ae62
Caleb Collert <c-collert.1@onu.edu>
2024-04-04 10:28:55 -0400

    completed isSortedDecreasing

--- P9-2/P9-2.cpp
+++ P9-2/P9-2.cpp
@@ -20,9 +20,15 @@ int main() {
 		cout << endl << "The vaules are sorted in increasing value\n";
 	}
 	else {
-		cout << endl << "The values are not sorted in increasing value.";
+		cout << endl << "The values are not sorted in increasing value.\n";
 	}
 
+	if (isSortedDecreasing(a)) {
+		cout << endl << "The vaules are sorted in decreasing value\n";
+	}
+	else {
+		cout << endl << "The values are not sorted in decreasing value.\n";
+	}
 }
 
 bool isSortedIncreasing(int values[6]) {
@@ -34,7 +40,7 @@ bool isSortedIncreasing(int values[6]) {
 			secondNumber++;
 		}
 	}
-	if (secondNumber == 6) {
+	if (secondNumber >= 6) {
 		return true;
 	}
 	else {
@@ -43,7 +49,20 @@ bool isSortedIncreasing(int values[6]) {
 	
 }
 bool isSortedDecreasing(int values[6]) {
-	return 0;
+	int firstNumber = 0;
+	int secondNumber = 1;
+	for (int i = 0; i < 6; i++) {
+		if (values[firstNumber] > values[secondNumber]) {
+			firstNumber++;
+			secondNumber++;
+		}
+	}
+	if (secondNumber >= 6) {
+		return true;
+	}
+	else {
+		return false;
+	}
 }
 bool hasAdjacentDupliciates(int values[6]) {
 	return 0;
